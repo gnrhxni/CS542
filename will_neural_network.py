abbe119eae309cbcb22922da3b10720abb64c964
pavlarez <palvarez@palvarez.net>
2014-04-22 15:24:58 +0000

    set it up to run multiple experiments with the same or different networks

--- will_neural_network.py
+++ will_neural_network.py
@@ -5,6 +5,7 @@ import re
 import sys
 import numpy
 import pybrain
+import pickle
 import math
 import time
 from nettalk_data import *
@@ -20,26 +21,24 @@ from pybrain.tools.shortcuts import buildNetwork
 
 from nettalk_modules import *
 
-print("Setting up network")
-ITERATIONS = 10
-modules = buildModules(NUMINPUTS, 80, NUMOUTPUTS)
-neural_network = buildnet(modules)
+
+ITERATIONS = 20
+
+def setup():
+    print("Setting up network")
+    modules = buildModules(NUMINPUTS, 80, NUMOUTPUTS)
+    neural_network = buildnet(modules)
 #IMPORTANT: IF YOU WANT TO SET YOUR WEIGHTS TO -0.3 to 0.3, please use the following 4 lines
-newWeights = np.random.uniform(-0.3, 0.3, len(neural_network.params))
+    newWeights = np.random.uniform(-0.3, 0.3, len(neural_network.params))
+    #pickle.dump(newWeights, open("weights.p","wb"))
+    #newWeights = pickle.load(open("weights.p","rb"))
 #print(newWeights)
-neural_network._setParameters(newWeights)
+    neural_network._setParameters(newWeights)
 #print(neural_network.params)
-print("Setting up trainer")
-trainer = BackpropTrainer(
-    neural_network, 
-	None, 
-    learningrate=1.0, 
-    verbose=False, 
-    batchlearning=True, 
-    weightdecay=0.0)
+    print("Setting up trainer")
+    return (neural_network, modules)
 
-
-def testOneWord(word, output=None):
+def testOneWord(neural_network, word, output=None):
    """ Return (phoneme_error, stress_error) lists
 
    The lists are parallel and indexed by letter position
@@ -65,18 +64,19 @@ def testOneWord(word, output=None):
                 char_pos = char_pos + 1
    return (phoneme_error, stress_error)
 
-def testWords(inputfile):
+def testWords(neural_network, inputfile):
     phoneme_error = list()
     stress_error = list()
     #loop through each word in our data, treating each one as a seperate dataset
     for word in dictionary(inputfile):
-        (pherrors, serrors) = testOneWord(word);
+        (pherrors, serrors) = testOneWord(neural_network, word);
         phoneme_error.extend(pherrors);
         stress_error.extend(serrors);
     print("Generalization: phoneme %.3f stress %.3f" % ( 1-np.mean(phoneme_error), 1-np.mean(stress_error)) )
+    return ( 1-np.mean(phoneme_error), 1-np.mean(stress_error))
     
 	  
-def trainNetwork():
+def trainNetwork(neural_network, trainer):
     phoneme_error = list()
     stress_error = list()
     #loop through each word in our data, treating each one as a seperate dataset
@@ -94,20 +94,35 @@ def trainNetwork():
                 char_pos+= 1
         trainer.setData(ds)
         err = trainer.train()
-        (pherrors, serrors) = testOneWord(word, output);
+        (pherrors, serrors) = testOneWord(neural_network, word, output);
         phoneme_error.extend(pherrors);
         stress_error.extend(serrors);
     print("Accuracy: phoneme %.3f stress %.3f" % ( 1-np.mean(phoneme_error), 1-np.mean(stress_error)) )
+    return ( 1-np.mean(phoneme_error), 1-np.mean(stress_error))
 
 	
 def main():
+  experiment = []
+  for beta in (0,0,0.0003,0.0003):
+   for r in (0.4, 0.6, 0.8):
+    (net, modules) = setup()
+    trainer = BackpropTrainer( net, None, learningrate=1.0, verbose=False, batchlearning=True, weightdecay=0.0)
+    modules['hidden'].beta = beta
+    modules['hidden'].r = r
+    print ("beta ", beta, " r ", r)
     for i in range(ITERATIONS):
-        start=time.time()
-        trainNetwork()
-        trained = time.time()
-        testWords('nettalk.data')
-        tested = time.time()
-        print("time to train ", trained - start, " to test ", tested - trained)
-        
+        data = [ beta, r, i ];
+        #start=time.time()
+        trainerror = trainNetwork(net, trainer)
+        data.extend(trainerror)
+        #trained = time.time()
+        if (0==(i+1)%5): 
+            testerror = testWords(net, 'nettalk.data')
+            data.extend(testerror)
+        #tested = time.time()
+        #print("time to train ", trained - start, " to test ", tested - trained)
+        experiment.append(data)
+  for i in experiment:
+     print i;
 
 main()
